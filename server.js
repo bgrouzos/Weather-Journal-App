978272a7c5c9cec9593853e4245f6dc145c0bb22
BrianG <bgrouzos@yahoo.com>
2022-01-05 20:57:00 -0600

    Add server.js initialization code and node modules

--- server.js
+++ server.js
@@ -2,18 +2,30 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000; //set the port to be used
+const server = app.listen(port, listening);
+
+//Listening function to make sure server is working
+function listening() {
+	console.log(`server running on ${port}`);
+}
