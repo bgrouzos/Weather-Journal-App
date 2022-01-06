68a815f4207f75a3afd0539c36ef8741439b64e8
BrianG <bgrouzos@yahoo.com>
2022-01-05 20:44:33 -0600

    Add API information

--- website/app.js
+++ website/app.js
@@ -1,4 +1,7 @@
 /* Global Variables */
+let baseURL = 'api.openweathermap.org/data/2.5/weather?zip='
+let zipCode = ''
+let apiKey = ',us&appid=0a31ee3c06242528db32c04a476eae93&units=imperial';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
