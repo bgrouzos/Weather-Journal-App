1e7b2e0eefb9d2f965bff33346258cb1ba629e6f
BrianG <bgrouzos@yahoo.com>
2022-01-06 21:11:34 -0600

    Add API communication

--- website/app.js
+++ website/app.js
@@ -1,8 +1,23 @@
 /* Global Variables */
-let baseURL = 'api.openweathermap.org/data/2.5/weather?zip='
-let zipCode = ''
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+let zipCode = '63033'
 let apiKey = ',us&appid=0a31ee3c06242528db32c04a476eae93&units=imperial';
+const newURL= baseURL+zipCode+apiKey
+console.log(newURL);
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+
+//Create async function to communicate with API
+const postData = async (url) => {
+	const res = await fetch(url);
+	//Call API
+	try {
+		const newData = await res.json();
+		console.log(newData);
+	}  catch(error) {
+		console.log("error", error);
+	}
+} 
